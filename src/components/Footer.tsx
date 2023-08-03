3ba6f3c74b6a640998fb643b4ba60463c0374585
Ralph Parin <trydonexz77@gmail.com>
2023-08-03 15:44:02 -0700

    add react version

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import extLink from "../app/assets/icons/external-link.svg";
+
+export default function Footer() {
+  return (
+    <footer className="text-center">
+      <div className="flex flex-row text-center justify-center">
+        Built using React and Tailwind.
+        <a
+          href="https://github.com/rparin/rparin.github.io"
+          target="_blank"
+          rel="noopener noreferrer">
+          <Image
+            className="invert w-auto h-3 2xl:h-10"
+            src={extLink}
+            alt="Source code for portfolio website on github"
+          />
+        </a>
+      </div>
+      ©️ 2023. Designed by Ralph Parin.
+    </footer>
+  );
+}
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import gitLogo from "../app/assets/logos/Contact/Github.svg";
+import inLogo from "../app/assets/logos/Contact/LinkedIn.svg";
+import gmailLogo from "../app/assets/logos/Contact/GMail.svg";
+
+const inInfo = {
+  href: "https://www.linkedin.com/in/rparin",
+  src: inLogo,
+  alt: "Linkedin Logo",
+};
+
+const gitInfo = {
+  href: "https://github.com/rparin",
+  src: gitLogo,
+  alt: "Github Logo",
+};
+
+const emailInfo = {
+  href: "mailto:r.parin070@gmail.com",
+  src: gmailLogo,
+  alt: "GMail Logo",
+};
+
+export default function Contact() {
+  const info = [inInfo, gitInfo, emailInfo];
+  var cInfos = [];
+  for (var i = 0; i < info.length; i++) {
+    cInfos.push(
+      <a href={info[i].href} target="_blank" rel="noopener noreferrer" key={i}>
+        <Image
+          className="p-1 w-14 2xl:w-52 2xl:p-4 h-auto drop-shadow-xl hover:p-0"
+          src={info[i].src}
+          alt={info[i].alt}
+        />
+      </a>
+    );
+  }
+  return (
+    <section
+      id="contact"
+      className="flex min-h-[90vh] flex-col text-center justify-center">
+      <h2>Contact Me</h2>
+      <p className="pb-1">Feel free to reach out! I don't bite.</p>
+      <div className="flex flex-wrap justify-center gap-6 2xl:gap-14">
+        {cInfos}
+      </div>
+    </section>
+  );
+}
