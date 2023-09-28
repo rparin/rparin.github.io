045b2210c9dcbadc983adf88a01d3832591c297d
Ralph Parin <r.parin070@gmail.com>
2023-09-27 23:34:39 -0700

    update constants

--- src/constants/NavLinks.tsx
+++ src/constants/NavLinks.tsx
@@ -1,18 +1,40 @@
-export const NavLinks = [
-  {
+const NavItems = {
+  projects: {
     id: "projects",
     title: "Projects",
   },
-  {
+  about: {
     id: "about",
     title: "About",
   },
-  {
+  contact: {
     id: "contact",
     title: "Contact",
   },
-  {
+  resume: {
     id: "resume",
     title: "Resume",
+    url: "",
+  },
+};
+
+const NavLinks = [
+  {
+    id: NavItems.projects.id,
+    title: NavItems.projects.title,
+  },
+  {
+    id: NavItems.about.id,
+    title: NavItems.about.title,
+  },
+  {
+    id: NavItems.contact.id,
+    title: NavItems.contact.title,
+  },
+  {
+    id: NavItems.resume.id,
+    title: NavItems.resume.title,
   },
 ];
+
+export { NavItems, NavLinks };
