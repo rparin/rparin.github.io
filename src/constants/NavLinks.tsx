adfceab804fde5dcd008d0147eb845752dba5160
Ralph Parin <r.parin070@gmail.com>
2023-09-27 19:15:23 -0700

    add constants files

---
+++ src/constants/NavLinks.tsx
@@ -0,0 +1,18 @@
+export const NavLinks = [
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+  {
+    id: "resume",
+    title: "Resume",
+  },
+];
