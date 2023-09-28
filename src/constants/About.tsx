e65e0782a0278c3e7195870751fcf4d769e8668d
Ralph Parin <r.parin070@gmail.com>
2023-09-27 19:15:23 -0700

    add constants files

---
+++ src/constants/About.tsx
@@ -0,0 +1,24 @@
+const AboutGen = {
+  First: "Ralph",
+  Last: "Parin",
+  email: "r.parin070@gmail.com",
+  titles: "Software Engineer, WebDeveloper, Gamer",
+  sent: "I enjoy",
+};
+
+const AboutSent = [
+  {
+    sent: "reading light novels.",
+  },
+  {
+    sent: "playing games.",
+  },
+  {
+    sent: "watching movies.",
+  },
+  {
+    sent: "trying new things.",
+  },
+];
+
+export { AboutGen, AboutSent };
