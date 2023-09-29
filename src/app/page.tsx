f0c00a08c1411fc5090de4977781064a6620f012
Ralph Parin <trydonexz77@gmail.com>
2023-09-29 01:23:25 -0700

    add theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import Project from "@/pages/Project";
 import Contact from "@/pages/Contact";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function App() {
   return (
     <main>
+      <ModeToggle />
       <Landing />
       <Navbar />
       <Project />
