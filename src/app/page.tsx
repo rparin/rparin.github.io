b6a463352be0cc2912d819ea484ef70d94a7fc83
Ralph Parin <r.parin070@gmail.com>
2023-09-29 23:17:44 -0700

    move navbar to ui folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Landing from "@/pages/Landing";
 import About from "@/pages/About";
 import Project from "@/pages/Project";
 import Contact from "@/pages/Contact";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/Footer";
 export default function App() {
   return (
