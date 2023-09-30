3f9b6c99fd104e5ab24ce7a220f09892fef534ee
Ralph Parin <r.parin070@gmail.com>
2023-09-29 20:12:20 -0700

    disable top nav on mobile devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <>
       <nav className="sticky top-0 bg-background drop-shadow-lg z-10 ">
-        <ul className="flex flex-row gap-10 md:gap-12 md:justify-center pt-12 pb-3 md:py-5 ml-5 pr-5 overflow-y-auto">
+        <ul className="flex flex-row gap-9 md:gap-12 md:justify-center py-5 ml-5 pr-5 overflow-y-auto">
           <li>
             <a className="text-xl" href="#projects">
               Projects
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -8,12 +8,12 @@ export default function Landing() {
     <section id="home" className="relative">
       <a
         href="#home"
-        className="select-none flex gap-1 text-center text-lg md:text-xl fixed top-2 left-5 md:left-10 z-50 px-2 hover:text-sky-400 active:text-accent dark:active:text-sky-400">
+        className="select-none flex gap-1 text-white md:text-black dark:text-white text-center text-lg md:text-xl absolute md:fixed top-2 left-5 md:left-10 z-50 px-2 hover:text-sky-400 active:text-accent dark:active:text-sky-400">
         <span className="text-base">&lt; </span>
         RP
         <span className="text-base"> /&gt;</span>
       </a>
-      <ThemeToggle className="fixed top-1 right-5 md:right-10 z-50 border-0 h-9 rounded-md px-3 md:h-11 md:px-8 bg-background hover:bg-background active:bg-accent dark:bg-background dark:hover:bg-background dark:active:bg-accent" />
+      <ThemeToggle className="absolute md:fixed top-1 right-5 md:right-10 z-50 border-0 h-9 rounded-md px-3 md:h-11 md:px-8 bg-background hover:bg-background active:bg-accent dark:bg-background dark:hover:bg-background dark:active:bg-accent" />
       <div className="absolute bottom-4 z-50 text-center w-full flex flex-col gap-1 md:gap-3">
         <h1 className="text-3xl md:text-4xl leading-10 md:leading-[3rem] font-semibold">
           {AboutGen.First} {AboutGen.Last}
