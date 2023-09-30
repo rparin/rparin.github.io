2796cd4cffef385fc4d723c0f9a939e18fc961c7
Ralph Parin <r.parin070@gmail.com>
2023-09-29 19:07:03 -0700

    add top nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,34 @@
 export default function Navbar() {
   return (
-    <nav className="">
-      <ul className="flex flex-row gap-10 md:gap-12 md:justify-center my-5 ml-5 pr-5 overflow-y-auto">
-        <li>
-          <a className="text-xl" href="#projects">
-            Projects
-          </a>
-        </li>
-        <li>
-          <a className="text-xl" href="#about">
-            About
-          </a>
-        </li>
-        <li>
-          <a className="text-xl" href="#contact">
-            Contact
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-xl"
-            href="https://raw.githubusercontent.com/rparin/rparin.github.io/main/public/Resume.pdf"
-            target="_blank"
-            rel="noopener noreferrer">
-            Resume
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="sticky top-0 bg-background drop-shadow-lg z-10 ">
+        <ul className="flex flex-row gap-10 md:gap-12 md:justify-center pt-12 pb-3 md:py-5 ml-5 pr-5 overflow-y-auto">
+          <li>
+            <a className="text-xl" href="#projects">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a className="text-xl" href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="text-xl" href="#contact">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-xl"
+              href="https://raw.githubusercontent.com/rparin/rparin.github.io/main/public/Resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer">
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
