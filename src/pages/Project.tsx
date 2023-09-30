2c7475e8aa9b4a3cc0c7bf575c2598131c60dd93
Ralph Parin <r.parin070@gmail.com>
2023-09-30 01:49:56 -0700

    Project: Add project images grid

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,9 +1,25 @@
+import { ProjectItems } from "@/constants/Projects";
+import Image from "next/image";
+
 export default function Project() {
+  const PItems = [];
+  for (const [k, project] of Object.entries(ProjectItems)) {
+    PItems.push(
+      <Image
+        className={project.span ? "col-span-2" : undefined}
+        src={project.img}
+        alt={project.alt}
+      />
+    );
+  }
+
   return (
     <section
       id="projects"
-      className="before:content-[''] before:h-10 before:block 2xl:before:h-32 flex min-h-screen flex-col">
-      <h2 className="text-center">Projects</h2>
+      className="before:content-[''] before:h-16 md:before:h-20 before:block">
+      <div className="grid auto-cols-auto md:grid-cols-4 xl:grid-cols-5 gap-3 mx-16">
+        {PItems}
+      </div>
     </section>
   );
 }
