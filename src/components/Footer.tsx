85c0f1408ccb29b62a926c0f5eb405eb905ae3c2
Ralph Parin <r.parin070@gmail.com>
2023-09-30 03:58:56 -0700

    Footer: decrease ext icon size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
           target="_blank"
           rel="noopener noreferrer">
           <Image
-            className="max-w-[1.5vh] dark:invert w-auto h-4"
+            className="max-w-[1.5vh] invert w-auto h-3/5"
             src={extLink}
             alt="External link to source code"
           />
