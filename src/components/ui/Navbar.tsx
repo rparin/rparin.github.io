89e25a496e3053d0bd1defad9b6249a7501b4e60
Ralph Parin <r.parin070@gmail.com>
2023-09-30 19:55:13 -0700

    Navbar: Change resume link to button

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,15 +1,13 @@
-import { NavItems } from "@/constants/NavLinks";
+import { NavItems, Resume } from "@/constants/NavLinks";
+import Image from "next/image";
+import { Icons } from "@/constants/Images";
 
 export default function Navbar() {
   const NavTags = [];
   for (const [k, value] of Object.entries(NavItems)) {
     NavTags.push(
-      <li key={value.id}>
-        <a
-          className="text-xl"
-          href={value?.url ? value?.url : "#" + value.id}
-          target={value?.url ? "_blank" : undefined}
-          rel={value?.url ? "noopener noreferrer" : undefined}>
+      <li key={value.id} className="mt-1">
+        <a className="text-xl" href={"#" + value.id}>
           {value.title}
         </a>
       </li>
@@ -18,9 +16,24 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="sticky top-0 bg-background drop-shadow-lg z-10 ">
-        <ul className="flex flex-row gap-9 md:gap-12 md:justify-center py-3 md:py-4 ml-5 pr-5 overflow-y-auto">
+      <nav className="sticky top-0 bg-background drop-shadow-lg z-10">
+        <ul className="flex flex-row gap-9 md:gap-11 md:justify-center py-3 md:py-4 ml-5 pr-5 overflow-y-auto">
           {NavTags}
+
+          <li>
+            <button className="text-text dark:text-text text-lg bg-primary dark:bg-primary p-0 m-0 rounded-md hover:bg-secondary dark:hover:bg-secondary">
+              <a href={Resume.url} className="flex flex-row">
+                <span className="p-0 pt-1 pl-3 pr-3">Resume</span>
+                <div className="flex justify-end">
+                  <Image
+                    className="bg-secondary w-10 h-10 rounded-r-md sm:p-2"
+                    src={Icons.download.img}
+                    alt={Icons.download.alt}
+                  />
+                </div>
+              </a>
+            </button>
+          </li>
         </ul>
       </nav>
     </>
