2335875947d4a4e0a6532b28ddd8aff2f0498879
Ralph Parin <trydonexz77@gmail.com>
2023-10-01 01:45:34 -0700

    TextImage: Decrease width

--- src/components/TextImage.tsx
+++ src/components/TextImage.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import Image, { StaticImageData } from "next/image";
 
 const TextImageVariants = cva(
-  "absolute bg-background rounded-lg line-clamp-2 md:line-clamp-none top-1/4 md:top-1/3 p-2 md:p-3 text-sm md:text-base md:max-w-lg lg:max-w-xl",
+  "absolute bg-background rounded-lg line-clamp-2 md:line-clamp-none top-1/4 md:top-1/3 p-2 md:p-3 text-sm md:text-base md:max-w-lg",
   {
     variants: {
       variant: {
