c935142afa154faada4d6db81ff6165eac879f0c
Ralph Parin <r.parin070@gmail.com>
2023-09-30 18:13:07 -0700

    Contact: Minimize page height

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -8,27 +8,27 @@ export default function Contact() {
     <section
       id={NavItems.contact.id}
       className="flex flex-col text-center before:content-[''] before:h-20 before:block mx-5 lg:mx-16">
-      <div className="min-h-[75vh]">
+      <div className="min-h-[45vh]">
         <h2 className=" font-normal mt-10">Get In Touch</h2>
 
-        <div className="flex flex-wrap justify-center items-center mt-10">
+        <div className="flex flex-wrap justify-center items-center gap-6 mt-10">
           <a href={"mailto:" + AboutGen.email}>
             <Image
-              className="max-h-[12vh] p-1 w-14 2xl:w-52 2xl:p-4 h-auto drop-shadow-xl hover:p-0"
+              className="max-h-[12vh] p-1 w-14 h-auto drop-shadow-xl hover:p-0"
               src={Icons.gmail.img}
               alt={Icons.gmail.alt}
             />
           </a>
           <a href={Socials.linkedIn} target="_blank" rel="noopener noreferrer">
             <Image
-              className="max-h-[12vh] p-1 w-14 2xl:w-52 2xl:p-4 h-auto drop-shadow-xl hover:p-0"
+              className="max-h-[12vh] p-1 w-14  h-auto drop-shadow-xl hover:p-0"
               src={Icons.linkedin.img}
               alt={Icons.linkedin.alt}
             />
           </a>
           <a href={Socials.github} target="_blank" rel="noopener noreferrer">
             <Image
-              className="max-h-[12vh] p-1 w-14 2xl:w-52 2xl:p-4 h-auto drop-shadow-xl hover:p-0 dark:invert"
+              className="max-h-[12vh] p-1 w-14 h-auto drop-shadow-xl hover:p-0 dark:invert"
               src={Icons.github.img}
               alt={Icons.github.alt}
             />
