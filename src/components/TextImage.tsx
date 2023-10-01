8f3a603a716dd753040c8dd38b5c0004096fc393
Ralph Parin <r.parin070@gmail.com>
2023-10-01 02:24:28 -0700

    TextImage: Change variant breakpoints

--- src/components/TextImage.tsx
+++ src/components/TextImage.tsx
@@ -4,12 +4,12 @@ import { cva, type VariantProps } from "class-variance-authority";
 import Image, { StaticImageData } from "next/image";
 
 const TextImageVariants = cva(
-  "absolute bg-background rounded-lg line-clamp-2 md:line-clamp-none top-1/4 md:top-1/3 p-2 md:p-3 text-sm md:text-base md:max-w-lg",
+  "absolute bg-background rounded-lg line-clamp-2 md:line-clamp-none top-1/4 md:top-1/3 p-2 md:p-3 text-sm md:text-base  md:max-w-lg",
   {
     variants: {
       variant: {
-        left: "left-[20%] md:left-[30%] lg:left-[35%]",
-        right: "right-[25%] lg:right-[33%]",
+        left: "left-[25%] md:left-[30%] lg:left-[38%]",
+        right: "right-[30%] lg:right-[35%]",
       },
     },
     defaultVariants: {
