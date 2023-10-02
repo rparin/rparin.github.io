90507f8321b3f2265c8987a9a3d10677e630e060
Ralph Parin <r.parin070@gmail.com>
2023-10-01 20:30:42 -0700

    Project: Implement span 2 hover

--- src/constants/Projects.tsx
+++ src/constants/Projects.tsx
@@ -10,12 +10,14 @@ import TEditor from "@project/TextEditor.png";
 
 const ProjectImages = {
   CTMap: {
+    name: "Clinical Trials Map",
     img: CTMap,
     alt: "Clinical Trials Map App Screenshot",
     span: true,
     url: "https://github.com/rparin/CTMap",
   },
   STracker: {
+    name: "Sleep Tracker",
     img: STracker,
     imgM: STrackerM,
     alt: "Sleep Tracker App Screenshot",
@@ -23,36 +25,42 @@ const ProjectImages = {
     url: "https://github.com/rparin/Sleep-Tracker",
   },
   Sudoku: {
+    name: "Sudoku",
     img: Sudoku,
     alt: "Sudoku Help Menu Screenshot",
     span: false,
     url: "https://github.com/rparin/Sudoku",
   },
   PPSim: {
+    name: "Prey Predator Sim",
     img: PPSim,
     alt: "Prey Predator Simulation Screenshot",
     span: false,
     url: undefined,
   },
   FViewer: {
+    name: "Font Viewer",
     img: FViewer,
     alt: "Font Viewer App Screenshot",
     span: true,
     url: "https://github.com/rparin/Font-Viewer",
   },
   GCalc: {
+    name: "Graphing Calculator",
     img: GCalc,
     alt: "Graphing Calculator App Screenshot",
     span: true,
     url: "https://github.com/rparin/Graphing-Calculator",
   },
   TEditor: {
+    name: "Text Editor",
     img: TEditor,
     alt: "Text Editor Screenshot",
     span: false,
     url: "https://github.com/rparin/JText-Editor",
   },
   SSearch: {
+    name: "Search Engine",
     img: SSearch,
     alt: "Search App Screenshot",
     span: true,
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,3 +1,4 @@
+import { Icons } from "@/constants/Images";
 import { ProjectItems } from "@/constants/Projects";
 import Image from "next/image";
 
@@ -16,8 +17,34 @@ export default function Project() {
 
   return (
     <section>
-      <div className="mt-5 grid auto-cols-auto md:grid-cols-4 xl:grid-cols-5 gap-3 mx-16">
-        {PItems}
+      <div className="mt-5 grid auto-cols-auto md:grid-cols-4 xl:grid-cols-5 gap-2 mx-16">
+        {/* Start of PImage */}
+        <div className="col-span-2 relative bg-background">
+          <div className="absolute opacity-0 min-[320px]:hover:opacity-100 text-text text-base font-semibold grid grid-cols-4 grid-rows-4 md:grid-cols-5 md:grid-rows-5 bg-background">
+            <Image
+              className="col-span-3 row-span-3 md:col-span-4 md:row-span-4"
+              src={ProjectItems[0].img}
+              alt={ProjectItems[0].alt}
+            />
+            <div className="row-span-3 md:row-span-4 flex flex-col justify-end items-center mx-2 gap-2">
+              <img
+                alt="build:passing"
+                src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+              />
+            </div>
+            <div className="col-span-4 md:col-span-5 flex flex-row justify-center items-center gap-2">
+              {ProjectItems[0].name}
+              <Image
+                className="w-auto h-5 md:h-7 dark:invert"
+                src={Icons.github.img}
+                alt={Icons.github.alt}
+              />
+            </div>
+          </div>
+          <Image src={ProjectItems[0].img} alt={ProjectItems[0].alt} />
+        </div>
+
+        {/* {PItems} */}
       </div>
     </section>
   );
