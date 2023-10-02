fb26d5a11562e596cfc31c2cd5315d240d3715e8
Ralph Parin <r.parin070@gmail.com>
2023-10-02 02:53:03 -0700

    Demo: Upload Project Demos

--- src/components/PImage.tsx
+++ src/components/PImage.tsx
@@ -3,17 +3,33 @@ import { pType } from "@/constants/Projects";
 import { SocialIcons } from "@/constants/Icons";
 
 export default function PImage(project: pType) {
-  const cn = "h-auto w-6 md:w-10 p-1";
+  const demoOn = false;
+  const icon_cn = "h-auto w-6 md:w-10 p-1";
   const codeTags = project.icons.map((icon) => {
     return (
       <Image
         key={icon.alt}
-        className={icon.invert ? cn + "invert-0 dark:invert" : cn}
+        className={icon.invert ? icon_cn + "invert-0 dark:invert" : icon_cn}
         src={icon.img}
         alt={icon.alt}
       />
     );
   });
+
+  const demo_cn =
+    "col-span-3 row-span-3 md:col-span-4 md:row-span-4 object-fill";
+  var demoDiv = (
+    <Image className={demo_cn} src={project.img} alt={project.alt} />
+  );
+  if (demoOn && project.demo) {
+    demoDiv = (
+      <Image
+        className={demo_cn}
+        src={project.demo.img}
+        alt={project.demo.alt}
+      />
+    );
+  }
 
   return (
     <div className={project.span ? "relative col-span-2" : "relative"}>
@@ -21,16 +37,12 @@ export default function PImage(project: pType) {
         href={project.url}
         target={project.url ? "_blank" : undefined}
         rel={project.url ? "noopener noreferrer" : undefined}>
-        <div className="grid grid-cols-4 grid-rows-4 md:grid-cols-5 md:grid-rows-5 absolute opacity-0 min-[320px]:hover:opacity-100 text-text text-base font-semibold bg-gradient-to-t from-background from-1% backdrop-blur-md">
-          <Image
-            className="col-span-3 row-span-3 md:col-span-4 md:row-span-4"
-            src={project.img}
-            alt={project.alt}
-          />
+        <div className="  grid grid-cols-4 grid-rows-4 md:grid-cols-5 md:grid-rows-5 absolute opacity-0 min-[320px]:hover:opacity-100 text-text text-base font-semibold bg-gradient-to-t from-background from-1% backdrop-blur-md">
+          {demoDiv}
           <div className="row-span-3 md:row-span-4 flex flex-col justify-start md:justify-center items-center mx-1 gap-2 overflow-y-auto max-h-24 md:max-h-none">
             {codeTags}
           </div>
-          <div className="col-span-4 md:col-span-5 flex flex-row justify-center items-center gap-2 truncate">
+          <div className=" col-span-4 md:col-span-5 flex flex-row justify-center items-center gap-2 truncate">
             {project.name}
             {project.url && (
               <Image
