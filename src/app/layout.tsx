05c7bded303d1f6c394ee450bd63a7844d570950
Ralph Parin <r.parin070@gmail.com>
2023-10-03 15:15:56 -0700

    Layout: Add page scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="scroll">
       <body className={font.className}>
         <ThemeProvider
           attribute="class"
