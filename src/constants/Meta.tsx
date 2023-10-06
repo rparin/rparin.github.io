73cfe36d29440d9ae99a7f9844a2263d184b68d0
Ralph Parin <r.parin070@gmail.com>
2023-10-05 20:43:32 -0700

    Metadata: Add og info

--- src/constants/Meta.tsx
+++ src/constants/Meta.tsx
@@ -1,12 +1,21 @@
-const Metadata = {
+const og = {
+  type: "website",
+  url: "https://rparin.github.io/",
   title: "Ralph Parin | Portfolio",
   description:
     "The website showcases some projects I've worked on and things about me. The technologies used to develop this website include React, Next.js, Tailwind CSS, and Shadcn-ui.",
+  siteName: "rparin.github.io",
   images: [
     {
       url: "https://raw.githubusercontent.com/rparin/rparin.github.io/main/Banner.png",
     },
   ],
+};
+
+const Metadata = {
+  title: og.title,
+  description: og.description,
+  openGraph: og,
 };
 
 export { Metadata };
