a976ab2c0e199acb5802bbdaf9be3187891fce02
Ralph Parin <r.parin070@gmail.com>
2023-10-05 20:05:23 -0700

    Metadata: Rephrase desc

--- src/constants/Meta.tsx
+++ src/constants/Meta.tsx
@@ -1,6 +1,6 @@
 const Metadata = {
   title: "Ralph Parin | Portfolio",
-  desc: "A website showcasing some projects I've done and things about me. Some technologies used to develop this website are: React, Next.js, Tailwind CSS, and Shadcn.",
+  desc: "The website showcases some projects I've worked on and things about me. The technologies used to develop this website include React, Next.js, Tailwind CSS, and Shadcn.",
 };
 
 export { Metadata };
