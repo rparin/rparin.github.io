438300525ae49f01371147a5a899d0d327af0415
Ralph Parin <r.parin070@gmail.com>
2023-10-05 20:34:57 -0700

    Manifest: Update const file

--- src/constants/Meta.tsx
+++ src/constants/Meta.tsx
@@ -1,9 +1,10 @@
 const Metadata = {
   title: "Ralph Parin | Portfolio",
-  desc: "The website showcases some projects I've worked on and things about me. The technologies used to develop this website include React, Next.js, Tailwind CSS, and Shadcn-ui.",
+  description:
+    "The website showcases some projects I've worked on and things about me. The technologies used to develop this website include React, Next.js, Tailwind CSS, and Shadcn-ui.",
   images: [
     {
-      url: "https://raw.githubusercontent.com/rparin/rparin.github.io/main/src/app/opengraph-image.png",
+      url: "https://raw.githubusercontent.com/rparin/rparin.github.io/main/Banner.png",
     },
   ],
 };
