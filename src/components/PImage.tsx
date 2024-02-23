aa6d3ac83c3a62157dea92566b75523a1676de03
Ralph Parin <r.parin070@gmail.com>
2024-02-22 18:49:22 -0800

    Project: add Blur placeholder image

--- src/components/PImage.tsx
+++ src/components/PImage.tsx
@@ -33,6 +33,9 @@ export default function PImage(project: pType) {
       width={100}
       height={100}
       loading="lazy"
+      placeholder="blur"
+      style={{ objectFit: "cover" }}
+      blurDataURL={project.img}
     />
   );
   if (demoOn && project.demo) {
@@ -44,6 +47,9 @@ export default function PImage(project: pType) {
         width={100}
         height={100}
         loading="lazy"
+        placeholder="blur"
+        style={{ objectFit: "cover" }}
+        blurDataURL={project.demo.img}
       />
     );
   }
@@ -85,6 +91,9 @@ export default function PImage(project: pType) {
         width={100}
         height={100}
         loading="lazy"
+        placeholder="blur"
+        style={{ objectFit: "cover" }}
+        blurDataURL={project.img}
       />
     </div>
   );
