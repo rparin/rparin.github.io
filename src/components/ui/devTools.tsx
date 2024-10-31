b311a91f84eef494d3a8f88ceb97649dc82a8d2e
Ralph Parin <r.parin070@gmail.com>
2024-10-31 15:29:21 -0700

    DevTools: Add keyboard navigation

--- src/components/ui/devTools.tsx
+++ src/components/ui/devTools.tsx
@@ -1,12 +1,25 @@
+"use client";
+
 import Image from "next/image";
 import { pType } from "@/constants/Projects";
+import { useRef, useState, useEffect } from "react";
 
 export default function DevTools({ tools }: { tools: pType["icons"] }) {
+  const [activeTool, setActiveTool] = useState(-1);
+  const devRefs = useRef<Array<HTMLAnchorElement>>([]);
+  const addToRefs = (el: HTMLAnchorElement) => {
+    if (el && !devRefs.current.includes(el)) {
+      devRefs.current.push(el);
+    }
+  };
+
   const icon_cn = "h-auto w-7 md:w-10 lg:w-14 md:p-1";
-  const devTools = tools.map((icon) => {
+  const devTools = tools.map((icon, index) => {
     return (
       <li key={icon.alt}>
         <a
+          ref={addToRefs}
+          tabIndex={-1}
           title={icon.name}
           href={icon.url}
           target={icon.url ? "_blank" : undefined}
@@ -20,8 +33,54 @@ export default function DevTools({ tools }: { tools: pType["icons"] }) {
       </li>
     );
   });
+
+  useEffect(() => {
+    if (activeTool != -1 && devRefs.current[activeTool]) {
+      devRefs.current[activeTool].focus();
+    }
+  }, [activeTool]);
+
+  const keyListNav = (e: React.KeyboardEvent<HTMLUListElement>) => {
+    switch (e.key) {
+      case "ArrowDown":
+        e.preventDefault();
+        if (activeTool === tools.length - 1) {
+          setActiveTool(0);
+        } else {
+          setActiveTool(activeTool + 1);
+        }
+        break;
+
+      case "ArrowUp":
+        e.preventDefault();
+        if (activeTool === 0) {
+          setActiveTool(tools.length - 1);
+        } else {
+          setActiveTool(activeTool - 1);
+        }
+        break;
+
+      case "Home":
+        e.preventDefault();
+        setActiveTool(0);
+        break;
+
+      case "End":
+        e.preventDefault();
+        setActiveTool(tools.length - 1);
+        break;
+
+      default:
+        //Nothing
+        break;
+    }
+  };
+
   return (
-    <ul className="p-scroll row-span-3 mx-1 mt-1 flex flex-col items-center justify-start gap-2 overflow-y-auto md:row-span-4 lg:mt-2">
+    <ul
+      tabIndex={0}
+      className="p-scroll row-span-3 mx-1 mt-1 flex flex-col items-center justify-start gap-2 overflow-y-auto md:row-span-4 lg:mt-2"
+      onKeyDown={keyListNav}>
       {devTools}
     </ul>
   );
