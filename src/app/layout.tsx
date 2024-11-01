09743cb0860ba7da3fe7c648094e5ae269021397
Ralph Parin <r.parin070@gmail.com>
2024-11-01 12:52:28 -0700

    ThemeToggle: Refactor, add aria role switch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning className="scroll">
+    <html lang="en" suppressHydrationWarning>
       <body className={font.className}>
         <ThemeProvider
           attribute="class"
--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -2,41 +2,24 @@
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useEffect } from "react";
-import { signal } from "@preact/signals";
+import { useState } from "react";
 import { cn } from "@/lib/utils";
 
-export const signalTheme = signal("light");
 export function ThemeToggle({ className }: { className?: string | undefined }) {
-  const { setTheme } = useTheme();
-
-  const setThemeHelper = (theme: "dark" | "light" | string) => {
-    setTheme(theme);
-    signalTheme.value = theme;
-  };
+  const { setTheme, theme } = useTheme();
+  const [checked, setCheck] = useState(false);
 
   const changeTheme = () => {
-    const savedTheme = window.localStorage.getItem("theme");
-    setThemeHelper(savedTheme == "dark" ? "light" : "dark");
+    setTheme(theme == "dark" ? "light" : "dark");
+    setCheck(!checked);
   };
-
-  useEffect(() => {
-    const savedTheme = window.localStorage.getItem("theme");
-    if (savedTheme) {
-      setThemeHelper(savedTheme);
-    } else {
-      const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-      setThemeHelper(isDark ? "dark" : "light");
-    }
-  });
 
   return (
     <button
-      aria-label="Toggle Dark Mode"
+      type="button"
       role="switch"
-      aria-checked={
-        window.localStorage.getItem("theme") == "dark" ? "true" : "false"
-      }
+      aria-checked={checked}
+      aria-label={`Toggle Theme`}
       className={cn(
         "border-input hover:text-accent-foreground h-10 border bg-background px-4 hover:bg-accent",
         className
