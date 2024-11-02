fcf67e7f637ce2050009cb3958422f3f0593ba04
Ralph Parin <r.parin070@gmail.com>
2024-11-02 15:19:45 -0700

    Test: ThemeToggle button click test

--- __tests__/unit/ThemeToggle.test.tsx
+++ __tests__/unit/ThemeToggle.test.tsx
@@ -1,4 +1,5 @@
-import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 import { axe, toHaveNoViolations } from "jest-axe";
 import { ThemeToggle } from "@/components/ui/ThemeToggle";
 
@@ -14,4 +15,19 @@ describe("testing ThemeToggle Component Accessibility", () => {
 test("it renders component unchanged", () => {
   const { container } = render(<ThemeToggle />);
   expect(container).toMatchSnapshot();
+});
+
+test("it should switch themes when button is clicked", async () => {
+  const user = userEvent.setup();
+  render(<ThemeToggle />);
+
+  const themeToggle = screen.getByRole("switch");
+
+  // Check if the checkbox is initially unchecked
+  expect(themeToggle).toHaveAttribute("aria-checked", "false");
+
+  //Click on theme toggle
+  await user.click(themeToggle);
+
+  expect(themeToggle).toHaveAttribute("aria-checked", "true");
 });
