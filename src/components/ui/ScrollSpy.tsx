fd8daca8d579dc8f705d757ca9799288f1e20598
Ralph Parin <r.parin070@gmail.com>
2024-12-24 15:49:15 -0800

    ScrollSpy: refactor

--- src/components/ui/ScrollSpy.tsx
+++ src/components/ui/ScrollSpy.tsx
@@ -13,14 +13,12 @@ export default function ScrollSpy({
   const [activeIndex, setActiveIndex] = useState<number>(0);
 
   children.map((child, index) => {
-    navIndex[index] = (child as React.ReactElement<{ id: string }>).props.id;
+    navIndex[index] = child.props.id;
   });
 
   useEffect(() => {
     const offSets = children.map((child, index) => {
-      return document.getElementById(
-        (child as React.ReactElement<{ id: string }>).props.id
-      )!.offsetTop;
+      return document.getElementById(child.props.id)!.offsetTop;
     });
     const handleScroll = (e: Event) => {
       var index = nearestIndex(window.scrollY, offSets, 0, offSets.length - 1);
